d8432c8f0f38aa587ce3e0e1862bdf6628db86e9
phamthanhx18 <phamthanhx18@gmail.com>
2023-07-13 16:18:36 +0700

    nap luyen tap 2

--- ss11-cau-truc-lap-2/bai-tap/luyen-tap-1/bai-8/main.js
+++ ss11-cau-truc-lap-2/bai-tap/luyen-tap-1/bai-8/main.js
@@ -1,7 +1,9 @@
 function start() {
-    let numberStart = +prompt("Bạn hãy nhập một số bất kì")
-    let result = Math.floor(Math.random() * ((numberStart + 10) - (numberStart + 20))) + (numberStart + 10);
+    let numberBe = +prompt("Bạn hãy nhập số bé nhất");
+    let numberLon = +prompt("Bạn hãy nhập số lớn nhất");
+    let result = Math.floor((Math.random() * numberLon) + numberBe);
     let count = 0;
+    console.log(result)
     do {
         let duDoan = +prompt("Dự đoán số may mắn là bao nhiêu?")
         if (result === duDoan) {
@@ -9,8 +11,12 @@ function start() {
             break;
         } else {
             count = count+1;
-            if (count<=3) {
-                alert("Bạn đã đoán sai, vui lòng Đoán lại");
+            if (count<3) {
+                if (duDoan<result) {
+                    alert("Bạn đã đoán sai, số may mắn lớn hơn số bạn dự đoán, vui lòng thử lại");
+                } else {
+                    alert("Bạn đã đoán sai, số may mắn bé hơn số bạn dự đoán, vui lòng thử lại");
+                }
             } else {
                 alert("Bạn đã vượt quá số lần đóa quy định, số mà hệ thống đưa ra là "+result);
                 break;
